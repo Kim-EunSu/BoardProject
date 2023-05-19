b5eb1f304e13f8c110f1482a9d0eb4c71670571e
진원 <89727516+z1nun@users.noreply.github.com>
2023-05-19 18:27:18 +0900

    chore : 빌드 환경 변경

--- every-board/next.config.js
+++ every-board/next.config.js
@@ -4,12 +4,5 @@ const nextConfig = {
     appDir: true,
   },
 };
-webpack: config => {
-  config.module.rules.push({
-    test: /\.svg$/i,
-    issuer: /\.[jt]sx?$/,
-    use: ["@svgr/webpack"],
-  });
-  return config;
-},
-  (module.exports = nextConfig);
+
+module.exports = nextConfig;
