735be38b8477e6454fd11c58e153f7d6ed5ee47e
진원 <89727516+z1nun@users.noreply.github.com>
2023-05-21 23:51:41 +0900

    chore : 서버 css 렌더링 필요 라이브러리 설치

--- every-board/next.config.js
+++ every-board/next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   experimental: {
     appDir: true,
+  },
+  // Next.js에서 Css-in-js 오류 해결
+  // https://velog.io/@motive05/Next.js%EC%97%90%EC%84%9C-styled-components-%EC%82%AC%EC%9A%A9%EB%A9%B4%EC%84%9C-%EC%83%88%EB%A1%9C%EA%B3%A0%EC%B9%A8%ED%95%98%EB%A9%B4-%EC%8A%A4%ED%83%80%EC%9D%BC%EC%9D%B4-%EC%82%AC%EB%9D%BC%EC%A7%80%EB%8A%94-%EC%97%90%EB%9F%AC
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
   },
 };
 
