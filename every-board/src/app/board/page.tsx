d203b43b8ba8a88f4d214dd9b1ac014ac3c86093
진원 <89727516+z1nun@users.noreply.github.com>
2023-06-02 17:47:15 +0900

    design : detail 페이지 제작

--- every-board/src/app/board/page.tsx
+++ every-board/src/app/board/page.tsx
@@ -3,15 +3,43 @@
 import PostCard from "@/components/PostCard";
 import Header from "@/components/Header";
 import { styled } from "styled-components";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Dashboard = () => {
+  const router = useRouter();
+  // const Navigation = () => {
+  //   const router = useRouter();
+  //   router.push("/board/detail");
+  // };
   return (
     <Main>
       <Header title="자유게시판" />
-      <PostCard />
-      <PostCard />
-      <PostCard />
-      <PostCard />
+      {/* <PostCard onClick={() => router.push("/board/detail")} /> */}
+      <Link
+        href="board/detail"
+        style={{ textDecoration: "none", color: "#000000" }}
+      >
+        <PostCard />
+      </Link>
+      <Link
+        href="board/detail"
+        style={{ textDecoration: "none", color: "#000000" }}
+      >
+        <PostCard />
+      </Link>
+      <Link
+        href="board/detail"
+        style={{ textDecoration: "none", color: "#000000" }}
+      >
+        <PostCard />
+      </Link>
+      <Link
+        href="board/detail"
+        style={{ textDecoration: "none", color: "#000000" }}
+      >
+        <PostCard />
+      </Link>
     </Main>
   );
 };
