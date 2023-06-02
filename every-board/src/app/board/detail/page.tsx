72bca77a74cd12629fded04bc5252d7bccc30586
진원 <89727516+z1nun@users.noreply.github.com>
2023-06-02 17:47:23 +0900

    design : detail 페이지 제작

--- every-board/src/app/board/detail/page.tsx
+++ every-board/src/app/board/detail/page.tsx
@@ -1,3 +1,25 @@
-export default function page() {
-  return <div>detail</div>;
-}
+"use client";
+
+import DetailCard from "@/components/PostCard";
+import Header from "@/components/Header";
+import { styled } from "styled-components";
+import PostCard from "@/components/PostCard";
+
+const Dashboard = () => {
+  return (
+    <Main>
+      <Header title="자유게시판" />
+      <PostCard detail={true} />
+    </Main>
+  );
+};
+
+const Main = styled.main`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  margin-bottom: 20px;
+`;
+
+export default Dashboard;
