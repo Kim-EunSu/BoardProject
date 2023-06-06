ec9d0d71f532c5971db8dfa77adaae7a7a42a91d
Kim-EunSu <kes73640@gmail.com>
2023-06-06 23:36:20 +0900

    Fix: Style-Components

--- every-board/src/app/board/post/page.tsx
+++ every-board/src/app/board/post/page.tsx
@@ -52,7 +52,7 @@ const Label = styled.label`
   margin-bottom: 10px;
 `;
 
-const Catagory = styled.button<{ isActive: Boolean }>`
+const Catagory = styled.button<{ $isActive: Boolean }>`
   position: absolute;
   top: 40px;
   right: 15px;
@@ -63,8 +63,8 @@ const Catagory = styled.button<{ isActive: Boolean }>`
   background: transparent;
   border: 2px solid #5429ff;
 
-  color: ${props => (props.isActive ? "#ffff" : "#5429ff")};
-  background: ${props => (props.isActive ? "#5429ff" : "transparent")};
+  color: ${props => (props.$isActive ? "#ffff" : "#5429ff")};
+  background: ${props => (props.$isActive ? "#5429ff" : "transparent")};
 `;
 
 const CategoryWrap = styled.div`
@@ -141,15 +141,15 @@ const InputBtnWrap = styled.div`
   }
 `;
 
-const InputBtn = styled.button<{ isSelected: Boolean }>`
+const InputBtn = styled.button<{ $isSelected?: Boolean }>`
   max-width: 150px;
   font-weight: bold;
   padding: 0.3rem 2rem;
   border-radius: 4px;
   background: transparent;
   border: 2px solid #5429ff;
-  color: ${props => (props.isSelected ? "#ffff" : "#5429ff")};
-  background: ${props => (props.isSelected ? "#5429ff" : "transparent")};
+  color: ${props => (props.$isSelected ? "#ffff" : "#5429ff")};
+  background: ${props => (props.$isSelected ? "#5429ff" : "transparent")};
 
   &:first-child {
     margin-right: 20px;
@@ -270,7 +270,7 @@ export default function page() {
               ref={categoryInput}
               placeholder="카테고리를 선택하세요."
             />
-            <Catagory isActive={isActive} onClick={toggleModal}>
+            <Catagory $isActive onClick={toggleModal}>
               카테고리
             </Catagory>
             <CategoryWrap>{showModal && <Category></Category>}</CategoryWrap>
@@ -335,7 +335,7 @@ export default function page() {
           <InputBtnWrap>
             <InputBtn
               type="submit"
-              isSelected={inputBtn === "취소"}
+              $isSelected={inputBtn === "취소"}
               onClick={() => {
                 handleInputButton("취소");
                 backBoard();
@@ -345,7 +345,7 @@ export default function page() {
             </InputBtn>
             <InputBtn
               type="submit"
-              isSelected={inputBtn === "작성"}
+              $isSelected={inputBtn === "작성"}
               onClick={() => {
                 handleInputButton("작성");
                 saveBoard();
