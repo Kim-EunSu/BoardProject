4c8ceaa48c3f1280fed792253cc48e6e4dc5f995
진원 <89727516+z1nun@users.noreply.github.com>
2023-06-07 00:38:28 +0900

    feat : Home화면과 Post화면 조건부 렌더링

--- every-board/src/components/Category.tsx
+++ every-board/src/components/Category.tsx
@@ -2,8 +2,49 @@
 
 import styled from "styled-components";
 import ButtonLayout from "./ButtonLayout";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
-const Category = () => {
+interface Props {
+  post?: string;
+  home?: string;
+  fn?: Function;
+}
+
+const Nav = styled.nav`
+  width: 350px;
+  background-color: #ffffff;
+  border-radius: 8px;
+  height: fit-content;
+
+  @media (min-width: 768px) {
+    width: 680px;
+  }
+  @media (min-width: 1080px) {
+    width: 980px;
+  }
+  @media (min-width: 1440px) {
+    width: 1200px;
+  }
+
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    text-align: center;
+    justify-content: space-between;
+    gap: 8px;
+    list-style: none;
+    padding: 12px;
+  }
+`;
+
+const Category = (props: Props) => {
+  const { post, home, fn } = props;
+  const [isPost, setPost] = useState<string | undefined>(home);
+  const router = useRouter();
+  useEffect(() => {
+    if (post) return setPost(post);
+  }, []);
   const categories = [
     "자유게시판",
     "IT",
@@ -41,17 +82,33 @@ const Category = () => {
         {categories.map((el, index) => {
           return (
             <li key={index}>
-              <ButtonLayout
-                text={`${el}`}
-                width="75px"
-                height="28px"
-                radius="8px"
-                color="var(--primary)"
-                background="#EFE9FF50"
-                fontSize="0.75rem"
-                border="none"
-                router={`/board?category=${el}`}
-              />
+              {home ? (
+                <ButtonLayout
+                  text={`${el}`}
+                  width="75px"
+                  height="28px"
+                  radius="8px"
+                  color="var(--primary)"
+                  background="#EFE9FF50"
+                  fontSize="0.75rem"
+                  border="none"
+                  onClick={() => router.push(`/board?category=${el}`)}
+                />
+              ) : (
+                <ButtonLayout
+                  text={`${el}`}
+                  width="75px"
+                  height="28px"
+                  radius="8px"
+                  color="var(--primary)"
+                  background="#EFE9FF50"
+                  fontSize="0.75rem"
+                  border="none"
+                  onClick={() => {
+                    fn ? fn(el) : undefined;
+                  }}
+                />
+              )}
             </li>
           );
         })}
@@ -61,30 +118,3 @@ const Category = () => {
 };
 
 export default Category;
-
-const Nav = styled.nav`
-  width: 350px;
-  background-color: #ffffff;
-  border-radius: 8px;
-  height: fit-content;
-
-  @media (min-width: 768px) {
-    width: 680px;
-  }
-  @media (min-width: 1080px) {
-    width: 980px;
-  }
-  @media (min-width: 1440px) {
-    width: 1200px;
-  }
-
-  ul {
-    display: flex;
-    flex-wrap: wrap;
-    text-align: center;
-    justify-content: space-between;
-    gap: 8px;
-    list-style: none;
-    padding: 12px;
-  }
-`;
