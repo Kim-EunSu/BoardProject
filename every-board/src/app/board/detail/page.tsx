00e07d50931d45afeb1010d0de3e1ea97a05b4e1
진원 <89727516+z1nun@users.noreply.github.com>
2023-06-10 12:17:07 +0900

    feat : detail page data fetching

--- every-board/src/app/board/detail/page.tsx
+++ every-board/src/app/board/detail/page.tsx
@@ -1,14 +1,26 @@
 "use client";
 
-import DetailCard from "@/components/PostCard";
-import Header from "@/components/Header";
 import styled from "styled-components";
 import PostCard from "@/components/PostCard";
+import LoadingPage from "@/components/LoadingPage";
+import ErrorPage from "@/components/ErrorPage";
+import { useGetDetailContent } from "@/utils/api";
 
 const Dashboard = () => {
+  // 상단 URL 쿼리로부터 contentId 가져오기
+  const urlParams = new URL(location.href).searchParams;
+  const contentId: string | null = urlParams.get("contentId");
+  const { data, isLoading, isError } = useGetDetailContent(contentId);
+  if (isLoading) {
+    return <LoadingPage />;
+  }
+  if (isError) {
+    return <ErrorPage />;
+  }
+
   return (
     <Main>
-      <PostCard detail={true} />
+      <PostCard detail={true} data={data} />
     </Main>
   );
 };
