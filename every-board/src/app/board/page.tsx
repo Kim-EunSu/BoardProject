2d0b64b6ab37d7bd33fa2e6ab9df43a1e4721d2e
진원 <89727516+z1nun@users.noreply.github.com>
2023-06-10 12:16:23 +0900

    feat : dashboard data fetching

--- every-board/src/app/board/page.tsx
+++ every-board/src/app/board/page.tsx
@@ -1,16 +1,27 @@
 "use client";
 
 import PostCard from "@/components/PostCard";
-import Header from "@/components/Header";
+import Loading from "@/components/Loading";
 import styled from "styled-components";
+import { useGetCategoryContent } from "@/utils/api";
+import type { ContentDetail } from "@/utils/type";
 
 const Dashboard = () => {
+  // 상단 URL 쿼리로부터 contentId 가져오기
+  const urlParams = new URL(location.href).searchParams;
+  const category: string | null = urlParams.get("category");
+  const { data, isLoading, isError } = useGetCategoryContent(category);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <Main>
-      <PostCard />
-      <PostCard />
-      <PostCard />
-      <PostCard />
+      {data &&
+        data.map((el: ContentDetail, index: number) => {
+          return <PostCard key={index} data={el} />;
+        })}
     </Main>
   );
 };
