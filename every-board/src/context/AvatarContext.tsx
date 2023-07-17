3939f4febc30cf709f90d870a8e322ba5c3635cc
Kim-EunSu <kes73640@gmail.com>
2023-07-17 12:09:47 +0900

    Feat: 프로필새로고침사라짐 수정

--- every-board/src/context/AvatarContext.tsx
+++ every-board/src/context/AvatarContext.tsx
@@ -1,34 +1,3 @@
-// "use client";
-
-// import React, { createContext, useContext, useState } from "react";
-
-// type AvatarContextType = {
-//   avatarImage: string | null;
-//   setAvatarImage: (image: string | null) => void;
-// };
-
-// const AvatarContext = createContext<AvatarContextType>({
-//   avatarImage: null,
-//   setAvatarImage: () => {},
-// });
-
-// export const useAvatar = () => {
-//   return useContext(AvatarContext);
-// };
-
-// type AvatarProviderProps = {
-//   children: React.ReactNode;
-// };
-
-// export const AvatarProvider: React.FC<AvatarProviderProps> = ({ children }) => {
-//   const [avatarImage, setAvatarImage] = useState<string | null>(null);
-
-//   return (
-//     <AvatarContext.Provider value={{ avatarImage, setAvatarImage }}>
-//       {children}
-//     </AvatarContext.Provider>
-//   );
-// };
 "use client";
 
 import React, { createContext, useContext, useState } from "react";
