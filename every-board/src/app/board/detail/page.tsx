0a1c45b099274970283e3d269e12d7001c3e9f10
Kim-EunSu <kes73640@gmail.com>
2023-07-31 09:21:26 +0900

    feat: 수정중

--- every-board/src/app/board/detail/page.tsx
+++ every-board/src/app/board/detail/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 "use client";
 
 import styled from "styled-components";
@@ -35,40 +34,4 @@ const Main = styled.main`
   height: fit-content;
 `;
 
-=======
-import styled from "styled-components";
-import PostCard from "@/components/PostCard";
-import LoadingPage from "@/components/LoadingPage";
-import ErrorPage from "@/components/ErrorPage";
-import { useSearchParams } from "next/navigation";
-import { useGetDetailContent } from "@/utils/api";
-
-const Dashboard = () => {
-  // 상단 URL 쿼리로부터 contentId 가져오기
-  const params = useSearchParams();
-  const contentId: string | null | undefined = params?.get("contentId");
-  const { data, isLoading, isError } = useGetDetailContent(contentId);
-  if (isLoading) {
-    return <LoadingPage />;
-  }
-  if (isError) {
-    return <ErrorPage />;
-  }
-
-  return (
-    <Main>
-      <PostCard detail={true} data={data} />
-    </Main>
-  );
-};
-
-const Main = styled.main`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 20px;
-  min-height: 100vh;
-  height: fit-content;
-`;
-
->>>>>>> Stashed changes
 export default Dashboard;
